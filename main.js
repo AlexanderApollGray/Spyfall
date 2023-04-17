66945631b74b080674989ceb8a4e9fcce0285aad
AlexanderApollGray <90724200+AlexanderApollGray@users.noreply.github.com>
2023-04-17 11:23:10 -0600

    work

--- main.js
+++ main.js
@@ -2,19 +2,19 @@
 
 // Control GUI Changes
 // HTML Variables
-let homeDiv = document.getElementById('home-div');
-let setUpDiv = document.getElementById('set-up-div');
-// let playDiv = document.getElementById('play-div');
-let setUpLink = document.getElementById('set-up-link');
-let startLink = document.getElementById('start-link');
+// let homeDiv = document.getElementById('home-div');
+// let setUpDiv = document.getElementById('set-up-div');
+// // let playDiv = document.getElementById('play-div');
+// let setUpLink = document.getElementById('set-up-link');
+// let startLink = document.getElementById('start-link');
 
-// Play Link Clicked
-setUpLink.addEventListener('click', displaySetUp)
+// // Play Link Clicked
+// setUpLink.addEventListener('click', displaySetUp)
 
-function displaySetUp() {
-    homeDiv.style.display = 'none';
-    setUpDiv.style.display = 'block';
-}
+// function displaySetUp() {
+//     homeDiv.style.display = 'none';
+//     setUpDiv.style.display = 'block';
+// }
 
 // // Start Link Clicked
 // startLink.addEventListener('click', displayStart);
@@ -25,18 +25,19 @@ function displaySetUp() {
 // }
 
 // Main Code
-// HTML Variables
+// HTML Variables 
 let playersAmount = document.getElementById('players').value;
-let spiesAmount = document.getElementById('spies').value;
-let startPlayers = document.getElementById('startPlayers');
+console.log(playersAmount)
+// let spiesAmount = document.getElementById('spies').value;
+// let startPlayers = document.getElementById('startPlayers');
 
-// Global Variable
-let locations = ['Amusement Park', 'Bank', 'Beach', 'Boat', 'Casino', 'Cemetary', 'Church', 'Circus',
-    'Embassy', 'Gas Station', 'Hospital', 'Hotel', 'Library', 'Military Base', 'Mine', 'Movie Theatre',
-    'Police Station', 'Restaurant', 'School', 'Spa', 'Stadium', 'University', 'Zoo'];
+// // Global Variable
+// let locations = ['Amusement Park', 'Bank', 'Beach', 'Boat', 'Casino', 'Cemetary', 'Church', 'Circus',
+//     'Embassy', 'Gas Station', 'Hospital', 'Hotel', 'Library', 'Military Base', 'Mine', 'Movie Theatre',
+//     'Police Station', 'Restaurant', 'School', 'Spa', 'Stadium', 'University', 'Zoo'];
 
-function displayStart() {
-    for (let p = 0; p < playersAmount; p++) {
-        startPlayers.innerHTML += `Player ${p}:`
-    }
-}
+// function displayStart() {
+//     for (let p = 0; p < playersAmount; p++) {
+//         startPlayers.innerHTML += `Player ${p}:`
+//     }
+// }
