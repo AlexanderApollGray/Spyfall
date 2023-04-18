8e4f9d39fdfba288b3ba56dcb30a9fc6c0eb15e3
AlexanderApollGray <90724200+AlexanderApollGray@users.noreply.github.com>
2023-04-18 11:23:13 -0600

    work

--- main.js
+++ main.js
@@ -1,33 +1,10 @@
 // Spyfall Javascript by Xander
 
-// Control GUI Changes
-// HTML Variables
-// let homeDiv = document.getElementById('home-div');
-// let setUpDiv = document.getElementById('set-up-div');
-// // let playDiv = document.getElementById('play-div');
-// let setUpLink = document.getElementById('set-up-link');
-// let startLink = document.getElementById('start-link');
-
-// // Play Link Clicked
-// setUpLink.addEventListener('click', displaySetUp)
-
-// function displaySetUp() {
-//     homeDiv.style.display = 'none';
-//     setUpDiv.style.display = 'block';
-// }
-
-// // Start Link Clicked
-// startLink.addEventListener('click', displayStart);
-
-// function displayStart() {
-//     setUpDiv.style.display = 'none';
-//     playDiv.style.display = 'block';
-// }
-
-// Main Code
 // HTML Variables 
+
+
 let playersAmount = document.getElementById('players').value;
-console.log(playersAmount)
+console.log(playersAmount);
 // let spiesAmount = document.getElementById('spies').value;
 // let startPlayers = document.getElementById('startPlayers');
 
