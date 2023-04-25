0a8835b4b25053b9e39a5dd10abb1cb2c3223cad
AlexanderApollGray <90724200+AlexanderApollGray@users.noreply.github.com>
2023-04-25 11:22:20 -0600

    work

--- main.js
+++ main.js
@@ -1,20 +1,45 @@
 // Spyfall Javascript by Xander
 
-// HTML Variables 
+// Global Variable
+let locations = ['Amusement Park', 'Bank', 'Beach', 'Boat', 'Casino', 'Cemetary', 'Church', 'Circus',
+    'Embassy', 'Gas Station', 'Hospital', 'Hotel', 'Library', 'Military Base', 'Mine', 'Movie Theatre',
+    'Police Station', 'Restaurant', 'School', 'Spa', 'Stadium', 'University', 'Zoo'];
 
+document.getElementById("btn").addEventListener("click", btnClicked);
 
-let playersAmount = document.getElementById('players').value;
-console.log(playersAmount);
-// let spiesAmount = document.getElementById('spies').value;
-// let startPlayers = document.getElementById('startPlayers');
+function btnClicked() {
+    // Make random players spies
+    let playersAmount = document.getElementById('players').value;
+    let spiesAmount = document.getElementById('spies').value;
+    let startPlayers = document.getElementById('start-players');
+    let random = '';
+    let randomLocation = randomElement(locations);
+    startPlayers.innerHTML = '';
+    for (let n = 0; n < spiesAmount; n++) {
+        random = randomInt(0, 4);
+    }
+    for (let p = 0; p < playersAmount; p++) {
+        if (p == random) {
+            startPlayers.innerHTML += `<p>Player ${p + 1}: Spy</p>`
+        } else {
+            startPlayers.innerHTML += `<p>Player ${p + 1}: ${randomLocation}</p>`
+        }
+        console.log(random);
+    }
+    console.log(randomLocation);
+    console.log(spiesAmount);
+}
 
-// // Global Variable
-// let locations = ['Amusement Park', 'Bank', 'Beach', 'Boat', 'Casino', 'Cemetary', 'Church', 'Circus',
-//     'Embassy', 'Gas Station', 'Hospital', 'Hotel', 'Library', 'Military Base', 'Mine', 'Movie Theatre',
-//     'Police Station', 'Restaurant', 'School', 'Spa', 'Stadium', 'University', 'Zoo'];
+const btn = document.getElementById('btn');
+const btn2 = document.getElementById('btn2');
+const select = document.getElementById('players');
+const select2 = document.getElementById('spies');
 
-// function displayStart() {
-//     for (let p = 0; p < playersAmount; p++) {
-//         startPlayers.innerHTML += `Player ${p}:`
-//     }
-// }
+btn.addEventListener('click', () => {
+    window.location = 'setup.html';
+});
+
+btn2.addEventListener('click', () => {
+    localStorage.setItem('select', select.value);
+    window.location = "play.html"
+});
