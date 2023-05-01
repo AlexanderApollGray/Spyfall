de7b6baa46afc73b90be38633d8752a31e4981c8
AlexanderApollGray <90724200+AlexanderApollGray@users.noreply.github.com>
2023-05-01 11:21:10 -0600

    work

--- main.js
+++ main.js
@@ -1,40 +1,41 @@
 // Spyfall Javascript by Xander
 
 // Global Variable
-let locations = ['Amusement Park', 'Bank', 'Beach', 'Boat', 'Casino', 'Cemetary', 'Church', 'Circus',
-    'Embassy', 'Gas Station', 'Hospital', 'Hotel', 'Library', 'Military Base', 'Mine', 'Movie Theatre',
-    'Police Station', 'Restaurant', 'School', 'Spa', 'Stadium', 'University', 'Zoo'];
+// let locations = ['Amusement Park', 'Bank', 'Beach', 'Boat', 'Casino', 'Cemetary', 'Church', 'Circus',
+//     'Embassy', 'Gas Station', 'Hospital', 'Hotel', 'Library', 'Military Base', 'Mine', 'Movie Theatre',
+//     'Police Station', 'Restaurant', 'School', 'Spa', 'Stadium', 'University', 'Zoo'];
 
-document.getElementById("btn").addEventListener("click", btnClicked);
+// document.getElementById("btn2").addEventListener("click", btnClicked);
 
-function btnClicked() {
-    // Make random players spies
-    let playersAmount = document.getElementById('players').value;
-    let spiesAmount = document.getElementById('spies').value;
-    let startPlayers = document.getElementById('start-players');
-    let random = '';
-    let randomLocation = randomElement(locations);
-    startPlayers.innerHTML = '';
-    for (let n = 0; n < spiesAmount; n++) {
-        random = randomInt(0, 4);
-    }
-    for (let p = 0; p < playersAmount; p++) {
-        if (p == random) {
-            startPlayers.innerHTML += `<p>Player ${p + 1}: Spy</p>`
-        } else {
-            startPlayers.innerHTML += `<p>Player ${p + 1}: ${randomLocation}</p>`
-        }
-        console.log(random);
-    }
-    console.log(randomLocation);
-    console.log(spiesAmount);
-}
+// function btnClicked() {
+//     // Make random players spies
+//     let playersAmount = document.getElementById('players').value;
+//     let spiesAmount = document.getElementById('spies').value;
+//     let startPlayers = document.getElementById('start-players');
+//     let random = '';
+//     let randomLocation = randomElement(locations);
+//     startPlayers.innerHTML = '';
+//     for (let n = 0; n < spiesAmount; n++) {
+//         random = randomInt(0, 4);
+//     }
+//     for (let p = 0; p < playersAmount; p++) {S
+//         if (p == random) {
+//             startPlayers.innerHTML += `<p>Player ${p + 1}: Spy</p>`
+//         } else {
+//             startPlayers.innerHTML += `<p>Player ${p + 1}: ${randomLocation}</p>`
+//         }
+//         console.log(random);
+//     }
+//     console.log(randomLocation);
+//     console.log(spiesAmount);
+// }
 
 const btn2 = document.getElementById('btn2');
 const select = document.getElementById('players');
 const select2 = document.getElementById('spies');
 
 btn2.addEventListener('click', () => {
-    localStorage.setItem('select', select.value);
+    localStorage.setItem('players', select.value);
+    localStorage.setItem('spies', select2.value);
     window.location = "play.html"
 });
--- play.js
+++ play.js
@@ -1,10 +1,39 @@
-// const btn = document.getElementById('btn');
-const span = document.getElementById('span');
+// const btn3 = document.getElementById('btn3');
+// const span = document.getElementById('span');
 
-// btn.addEventListener('click', () => {
-//     window.location = 'index1.html';
+// btn3.addEventListener('click', () => {
+//     window.location = 'home.html';
 // });
 
-window.onload = function () {
-    span.innerHTML = localStorage.getItem('input');
+// window.onload = function () {
+// span.innerHTML = localStorage.getItem('select');
+// }
+// Global Variable
+let locations = ['Amusement Park', 'Bank', 'Beach', 'Boat', 'Casino', 'Cemetary', 'Church', 'Circus',
+    'Embassy', 'Gas Station', 'Hospital', 'Hotel', 'Library', 'Military Base', 'Mine', 'Movie Theatre',
+    'Police Station', 'Restaurant', 'School', 'Spa', 'Stadium', 'University', 'Zoo'];
+
+document.getElementById("btn3").addEventListener("click", btnClicked);
+
+function btnClicked() {
+    // Make random players spies
+    let playersAmount = localStorage.getItem('players');
+    let spiesAmount = localStorage.getItem('players');
+    let startPlayers = document.getElementById('start-players');
+    let random = '';
+    let randomLocation = randomElement(locations);
+    startPlayers.innerHTML = '';
+    for (let n = 0; n < spiesAmount; n++) {
+        random = randomInt(0, 4);
+    }
+    for (let p = 0; p < playersAmount; p++) {
+        if (p == random) {
+            startPlayers.innerHTML += `<p>Player ${p + 1}: Spy</p>`
+        } else {
+            startPlayers.innerHTML += `<p>Player ${p + 1}: ${randomLocation}</p>`
+        }
+        console.log(random);
+    }
+    console.log(randomLocation);
+    console.log(spiesAmount);
 }
